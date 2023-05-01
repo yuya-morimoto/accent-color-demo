10a3b881a64cf404ae732b2a9486a84dcb0b0e2b
yuya-morimoto <gyu2243dev@gmail.com>
2023-05-01 14:31:41 +0900

    setting accent color

--- accent-color-demo/ContentView.swift
+++ accent-color-demo/ContentView.swift
@@ -8,12 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowAlert = false
+    @State private var isShowActionSheet = false
+    @State private var isConfirmationDialog = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            // MARK: - Alert
+
+            Button("Alert") {
+                isShowAlert = true
+            }
+            .alert(isPresented: $isShowAlert) {
+                Alert(
+                    title: Text("Current Location Not Available"),
+                    message: Text("Your current location can’t be " +
+                        "determined at this time.")
+                )
+            }.padding()
+
+            // MARK: - Action Sheet
+
+            Button("ActionSheet") {
+                isShowActionSheet = true
+            }
+            .actionSheet(isPresented: $isShowActionSheet) {
+                ActionSheet(title: Text("Resume Workout Recording"),
+                            message: Text("Choose a destination for workout data"),
+                            buttons: [
+                                .cancel(),
+                                .default(
+                                    Text("Append to Current Workout"),
+                                    action: {}
+                                ),
+                                .destructive(
+                                    Text("Overwrite Current Workout"),
+                                    action: {}
+                                )
+                            ])
+            }.padding()
+
+            // MARK: - Confirmation Dialog
+
+            Button("Import File") {
+                isConfirmationDialog = true
+            }
+            .confirmationDialog(
+                "Are you sure you want to import this file?",
+                isPresented: $isConfirmationDialog
+            ) {
+                Button {
+                    // Handle import action.
+                } label: {
+                    Text("""
+                    Confirmation Dialog Sample!
+                    """)
+                }
+                Button("Cancel", role: .cancel) {}
+            }.padding()
         }
         .padding()
     }
--- accent-color-demo/accent_color_demoApp.swift
+++ accent-color-demo/accent_color_demoApp.swift
@@ -7,8 +7,18 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).tintColor = UIColor(Color("AccentColor"))
+        return true
+    }
+}
+
 @main
 struct accent_color_demoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
